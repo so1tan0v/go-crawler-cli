4bc32d72bfbe4cf1b92cc311760d272221a7be80
Солтанов Александр Назимович <a.soltanov@k2t.app>
2026-02-17 22:08:15 +0300

    fix: CI

---
+++ crawler/api.go
@@ -0,0 +1,13 @@
+package crawler
+
+import "code/src/domain"
+
+type Options = domain.Options
+
+type Report = domain.AnalyzeResult
+
+type Page = domain.Page
+type SEO = domain.SEO
+type BrokenLink = domain.BrokenLink
+type Asset = domain.Asset
+
